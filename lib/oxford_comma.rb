6b21081ce41e63405a2e5733aa67a216dd061e4b
Aurora Avery <aurora.avery@yahoo.com>
2020-04-22 03:22:56 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,9 @@
 def oxford_comma(array)
-  array = [""]
 
-end
-def oxford_comma(array)
-  array = [""]
-
-end
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif 2 < array.length
+    array[-1].insert(0, "and ")
+  end
+  array.join(", ")    
+end 
